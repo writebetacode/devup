e03a2e86abb0489f3302252186073a19bcce6ff0
writebetacode <scholle.mv@protonmail.com>
2022-02-06 04:01:11 -0500

    setting the base for being able to create symlinks by just adding them to the associative array

--- mac-os/cmd/set-dot-files.zsh
+++ mac-os/cmd/set-dot-files.zsh
@@ -1,6 +1,39 @@
 #! /bin/zsh
 
-echo -e "creating symlinks for all of my dot-files\n"
+HOME_WD="$(dirname "$(pwd)")/home"
 
-mkdir ~/.config
-ln -s ./.config/kitty ~/config/
+if [ ! -d "$HOME/.config" ]; then
+  mkdir ~/.config
+fi
+
+function set_symlink(){
+  echo "creating symlink: $1 -> $2"
+  ln -s $1 $2
+}
+
+declare -A dot_files
+
+dot_files[kitty]=.config/
+dot_files[.gitignore]=.gitignore
+dot_files[.zprofile]=.zprofile
+dot_files[.zshrc]=.zshrc
+
+for from to in ${(kv)dot_files}; do
+  to_path=~/$to
+
+  if [ -d $to_path ]; then
+    from_path=$HOME_WD/$to$from
+  else
+    from_path=$HOME_WD/$from
+  fi
+
+  if [ -d $to_path ]; then
+    if [ ! -d $to_path$from ]; then
+      set_symlink $from_path $to_path
+    fi
+  else
+    if [ ! -f $to_path ]; then
+      set_symlink $from_path $to_path
+    fi
+  fi
+done
