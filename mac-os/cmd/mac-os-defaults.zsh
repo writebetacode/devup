4f23709e250dfcc5d88cdf8f5f493de307488484
Michael Scholle <5696777+writebetacode@users.noreply.github.com>
2023-04-22 06:33:13 -0400

    chore(readme): updating the mac-os cmd with even more values

--- mac-os/cmd/mac-os-defaults.zsh
+++ mac-os/cmd/mac-os-defaults.zsh
@@ -3,8 +3,21 @@
 echo -e "accepting xcodebuild license\n"
 sudo xcodebuild -license accept
 
-echo -e "setting finder to show all hidden files\n"
-defaults write com.apple.finder AppleShowAllFiles -boolean true; killall Finder
-
+# TODO: come back and see if this matters anymore with my upgraded monitors 
 echo -e "setting up non-fuzzy text for mojave\n"
 defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO
+
+echo -e "setting Finder to show all hidden files\n"
+defaults write com.apple.finder AppleShowAllFiles -boolean true; killall Finder
+
+echo -e "setting Finder to show all File extensions\n"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+echo -e "setting Finder to show the path par\n"
+defaults write com.apple.finder ShowPathbar -bool true
+
+echo -e "setting Finder to show a status bar\n"
+defaults write com.apple.finder ShowStatusBar -bool true
+
+echo -e "disabling the creation of pesky .DS_Store files\n"
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
