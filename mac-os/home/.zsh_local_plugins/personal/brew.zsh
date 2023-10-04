27def7a818df0207bb0c38be369e304f65a98e38
writebetacode <mscholle@centegix.com>
2023-10-04 11:30:53 -0400

    updating brew to include autocomplete

--- mac-os/home/.zsh_local_plugins/personal/brew.zsh
+++ mac-os/home/.zsh_local_plugins/personal/brew.zsh
@@ -1,6 +1,12 @@
 # setting up brew so that it is loaded for our current environment
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
+if type brew &>/dev/null
+then
+  FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+fi
+
+
 # add in go bin into path
 export GOBIN=$HOME/go/bin
 if [ -d $GOBIN ]; then
--- mac-os/home/.zshrc
+++ mac-os/home/.zshrc
@@ -1,3 +1,6 @@
 source ~/.zsh_local_plugins/personal/brew.zsh
 source ~/.zsh_local_plugins/personal/asdf.zsh
 source ~/.zsh_local_plugins/personal/aliases.zsh
+
+autoload -Uz compinit
+compinit
